91079a500d5189b7e20fabc4f960d6abe4a9f601
CapO <79900257+CapO@users.noreply.github.com>
2021-06-01 19:09:09 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,25 @@
+<footer class="footer container-fluid text-center">   
+              <div class="container">
+            <div class="row footer-text">
+                <div class="col-sm-4">
+         <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/Group%2043.png" alt="media">           
+       </div><!--col-sm-4-->    
+                
+     <div class="col-sm-4">
+                    
+    <p class="p-text">Fremantle Center Concert Series</p>
+       </div><!--col-sm-4--> 
+                
+      <div class="col-sm-4">
+           <img src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/Group%2044.png" alt="media"> 
+       </div><!--col-sm-4--> 
+                
+          </div> <!--row-->
+     </div> <!--container-->
+
+
+    </footer>
+
+
+
+<?php wp_footer(); ?>
