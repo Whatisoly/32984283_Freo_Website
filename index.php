0aeec30785eb8aa8dccf3af8c72aeb12a9b1aab9
CapO <79900257+CapO@users.noreply.github.com>
2021-06-01 19:09:24 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+<div class="container-fluid">
+        <div class="container">
+      <div class="row justify-content-around">
+               <h3 class="hh3">Music Act Range</h3> 
+    <div class="col-md-6">
+         <div class="text-1">
+    <h4>Electro</h4>
+    <p class="p-text">Come appreciate good electro music. That will give you the energy for anything.</p>
+      <p class="click"><a href="genres.html" target="_self">Click here for more</a></p>
+  </div>
+  <img class="img-responsive img-1" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/electro-band.png" alt="Electro-band">
+</div>
+          
+    <div class="col-md-6">
+<!--       <div class="container">-->
+  <img class="img-responsive img-2" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/classic-band.png" alt="Classic-band">
+  <div class="text-2">
+    <h4>Classic</h4>
+    <p class="p-text">Many various Classic artists, who will make you revisite your classical.</p>
+     <p class="click"><a href="genres.html" target="_self">Click here for more</a></p>
+  </div>
+<!--</div>-->
+    </div>
+            </div> 
+            
+   <div class="row justify-content-around">
+           <div class="col-md-6">
+         <div class="text-3">
+    <h4>Rock</h4>
+    <p class="p-text">Which style will you prefer? Indian rock, Hard rock, or Classic rock?</p>
+       <p class="click"><a href="genres.html" target="_self">Click here for more</a></p>
+  </div>
+  <img class="img-responsive img-3" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/rock-band.png" alt="Rock-band">
+</div>
+          
+    <div class="col-md-6">
+<!--       <div class="container">-->
+  <img class="img-responsive img-4" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/jazz-band.png" alt="Jazz-band">
+  <div class="text-4">
+    <h4>Jazz</h4>
+    <p class="p-text">For the fan of brass instrument. You will find your happiness there.</p>
+      <p class="click"><a href="genres.html" target="_self">Click here for more</a></p>
+  </div>
+<!--</div>-->
+    </div>
+            </div>  
+            </div>
+        </div>
+      
+      <div class="container-fluid bg-2">
+           <div class="container">
+          <h2>Contact/ Map</h2>
+    <div class="col-md-6">
+        <p class="p-text1">1 Finnerty Street, Fremantle WA 6160<br>PO Box 891, Fremantle WA 6959<br>08 9432 9555<br>ArtsCentre@fremantle.wa.gov.au</p>
+    </div>
+    <div class="col-md-6">
+      <img class="img-responsive img-5" alt="Map" src="ihttp://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/Capture.PNG">
+    </div>
+  </div>
+          </div> 
+
+
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
