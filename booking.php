824b6927a2f23ff53f0cefc332f5a0e2270f5b2e
CapO <79900257+CapO@users.noreply.github.com>
2021-06-02 15:51:55 +0800

    Create booking.php

---
+++ booking.php
@@ -0,0 +1,125 @@
+<?php
+    /*
+    Template Name: booking
+    */
+    ?>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+       <div class="container-fluid bg-4">
+        <div class="container">
+      <div class="row justify-content-around">
+               <h3 class="hhh3">Booking</h3> 
+            </div>
+           </div>
+      
+      
+      
+      
+      
+      
+    <div class="container-fluid">
+        <div class="container">
+      <div class="row justify-content-around">
+    <div class="col-md-6">
+         <div class="text-2-1">
+    <p class="p-textb">It is time to discover your soul-music.<br> Get tickets today before it's too late</p>
+  </div>
+
+</div>
+          
+      <div class="col-md-6">
+         <div class="text-2-2">
+    <p class="p-textc">Seating option: Front stage</p>
+  </div>
+
+</div>
+   </div> 
+       </div>
+          </div>
+      
+  <div class="container-fluid">
+        <div class="container">
+      <div class="row justify-content-around">
+    <div class="col-md-6">
+         <div class="text-2-3">
+    <p class="p-textc">Username:</p>
+  </div>
+
+</div>
+          
+      <div class="col-md-6">
+         <div class="text-2-4">
+    <p class="p-textc">Middle</p>
+  </div>
+
+</div>
+   </div> 
+       </div>
+          </div>
+      
+ <div class="container-fluid">
+        <div class="container">
+      <div class="row justify-content-around">
+    <div class="col-md-6">
+         <div class="text-2-3">
+    <p class="p-textc">Email:</p>
+  </div>
+
+</div>
+          
+      <div class="col-md-6">
+         <div class="text-2-4">
+    <p class="p-textc">Balcony</p>
+  </div>
+
+</div>
+   </div> 
+       </div>
+          </div>
+      
+      
+ <div class="container-fluid">
+        <div class="container">
+      <div class="row justify-content-around">
+    <div class="col-md-6">
+         <div class="text-2-3">
+    <p class="p-textc">Phone Number:</p>
+  </div>
+
+</div>
+          
+      <div class="col-md-6">
+         <div class="text-2-4">
+    <p class="p-textc">Other</p>
+  </div>
+
+</div>
+   </div> 
+       </div>
+          </div>
+      
+      
+ <div class="container-fluid">
+        <div class="container">
+      <div class="row justify-content-around">
+    <div class="col-md-6">
+         <div class="text-2-3">
+    <p class="p-textc">Number of Tickets:</p>
+  </div>
+
+</div>
+          
+      <div class="col-md-6">
+         <div class="text-2-5">
+    <p class="p-textc">Any questions?</p>
+  </div>
+
+</div>
+   </div> 
+       </div>
+          </div>
+           
+           
+           <a href="#" class="btn btn-default btn-lg1">Get Ticket</a>
+           
+      </div>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
